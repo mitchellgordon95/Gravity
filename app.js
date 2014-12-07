89f00c016a976da988f6f1c1a6a52ebf0ebcfb9a
Mitchell Gordon <mitchell.gordon95@gmail.com>
2014-12-06 22:56:39 -0600

    Commenting out logging

--- app.js
+++ app.js
@@ -12,12 +12,12 @@ var nextID = 0;
 io.hosts = new Array();
 // When the host joins, save it for later
 io.sockets.on('connection', function(socket) {
-	console.log('connection');
+	//console.log('connection');
 
 	// This socket is a host.
 	socket.on('startup_host', function (keyword, callback) {
 		if (keyword) {
-			console.log('Host connected with "' + keyword + '"');
+			//console.log('Host connected with "' + keyword + '"');
 			
 			// If a host is already using this keyword, return no success
 			if (io.hosts[keyword]) {
@@ -30,7 +30,7 @@ io.sockets.on('connection', function(socket) {
 
 			// When the host disconnects,
 			socket.on('disconnect', function () {
-				console.log('Host disconnected.');
+				//console.log('Host disconnected.');
 				io.hosts[keyword] = 0;
 			});
 		}
@@ -38,7 +38,7 @@ io.sockets.on('connection', function(socket) {
 
 	// When a client joins, assign it an ID
 	socket.on('client_join', function (keyword, callback) {
-		console.log('Client trys to join with "' + keyword + '"');
+		//console.log('Client trys to join with "' + keyword + '"');
 		if (io.hosts[keyword]) {
 			console.log('Client added to host with "' + keyword + '" and ID '+nextID);
 			// TODO - Move this somewhere better
@@ -56,14 +56,14 @@ io.sockets.on('connection', function(socket) {
 
 		// When a client leaves, tell the host
 		socket.on('disconnect', function() {
-			console.log('Client leaves "' + keyword + '" and ID ' + socket.clientID);
+			//console.log('Client leaves "' + keyword + '" and ID ' + socket.clientID);
 			if (io.hosts[keyword])
 				io.hosts[keyword].emit('remove_planet', socket.clientID);
 		});
 	});
 	// When a client moves, tell the host to update its position.
 	socket.on('input_event', function(clientID, keyword, input) {
-		console.log(input);
+		//console.log(input);
 		if (io.hosts[keyword])
 			io.hosts[keyword].emit('move_planet', clientID, input);
 	});
