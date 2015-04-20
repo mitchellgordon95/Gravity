0dc85c322fca253a1bc8754ce28da575c69da1ad
Mitchell Gordon <mitchell.gordon95@gmail.com>
2015-04-19 23:53:24 -0500

    The mobile controller wasn't working if you pressed the button more that once

--- mobile-controller/www/js/mobile_controller.js
+++ mobile-controller/www/js/mobile_controller.js
@@ -1,10 +1,11 @@
 
-var server;
+var server = io.connect("http://gravity.mitchgordon.me");
 
 var clientID;
 var tilt_output = document.getElementById("tilt_output");
 var keyword;
-			
+var connected = false;
+		
 document.getElementById('join_game').onclick = function () {
 	if(document.getElementById('keyword').value == "") {
 		alert("Please enter a keyword.");
@@ -12,13 +13,8 @@ document.getElementById('join_game').onclick = function () {
 	else {
 		keyword = document.getElementById('keyword').value;
 		
-		
-		server = io.connect("http://gravity.mitchgordon.me");
-		
-		// When we connect to the server
-		server.on('connect', function() {
-		  console.log('Connected to server.');
-		  server.emit('client_join', keyword, function(ID, color) {
+		if ( connected ) {
+			server.emit('client_join', keyword, function(ID, color) {
 			  // If we failed, it's probably because the keyword isn't being used
 			  if (ID == -1) {
 				navigator.notification.alert("Sorry, no games with that keyword exist.", function(){});
@@ -32,21 +28,31 @@ document.getElementById('join_game').onclick = function () {
 			  document.body.style.backgroundColor = "#" + color.toString(16);
 			  document.getElementById("clientID").innerHTML = "You are Player " + clientID;
 			  console.log('Joined game with ID ' + ID + ' and color ' + color.toString(16));
-		  });
-		});
-
-		// If we can't connect to the server
-		server.on('connect_error', function () {
-			navigator.notification.alert("Server connection error. Please try again later.", function(){});
-		});
-		
-		// For tilt support
-		window.addEventListener('deviceorientation', function(data){
 			
-			var cursors = {type: 'tilt', beta: data.beta, gamma: data.gamma};
-				
-			server.emit('input_event', clientID, keyword, cursors);
-		}, true);
+			    // For tilt support
+				window.addEventListener('deviceorientation', function(data){
+					
+					var cursors = {type: 'tilt', beta: data.beta, gamma: data.gamma};
+						
+					server.emit('input_event', clientID, keyword, cursors);
+				}, true);
+		  });
+		}
+		else {
+			navigator.notification.alert("Not yet connected to server. Please wait.", function(){});
+		}
+
 	}
 }
 
+//When we connect to the server
+server.on('connect', function() {
+	connected = true;
+  console.log('Connected to server.');
+  
+});
+
+// If we can't connect to the server
+server.on('connect_error', function () {
+	navigator.notification.alert("Server connection error. Please try again later.", function(){});
+});
